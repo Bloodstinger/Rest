8c798d1bf6692643435ba6481f81c6a8bf02eaea
Bloodstinger <47367238+Bloodstinger@users.noreply.github.com>
2019-08-23 16:24:53 +0300

    Update UserController.java

--- .gitignore/src/main/java/academy/mate/client/controller/UserController.java
+++ .gitignore/src/main/java/academy/mate/client/controller/UserController.java
@@ -11,15 +11,16 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class UserController {
 
+    private final String SERVER_URL = "http://localhost:8080/encodeUser";
+
     @GetMapping("/getUser")
-    public User getUser(@RequestParam(name = "name", required = false, defaultValue = "testUser") String username,
+    public User getUser(@RequestParam(name = "name", required = false,
+            defaultValue = "testUser") String username,
                         @RequestParam(name = "password", required = false,
                                 defaultValue = "testPassword") String password) {
         RestTemplate template = new RestTemplate();
-        String url = "http://localhost:8080/encodeUser";
         HttpEntity<User> request = new HttpEntity<>(new User(username, password));
-        ResponseEntity<User> user = template.postForEntity(url, request, User.class);
-        System.out.println(user.getBody());
+        ResponseEntity<User> user = template.postForEntity(SERVER_URL, request, User.class);
         return user.getBody();
     }
 }
